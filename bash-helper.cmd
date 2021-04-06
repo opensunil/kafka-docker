b2604b5d5a0ad77922d5ea6a48d1caecb9b342f0
Sunil Varghese <opensunil@gmail.com>
2021-04-06 17:07:38 -0400

    expose ports on host

--- bash-helper.cmd
+++ bash-helper.cmd
@@ -1,4 +1,4 @@
-alias dstart='sudo docker run --name kafka --rm -td kafkarm'
+alias dstart='sudo docker run -p 2181:2181 -p 2888:2888 -p 3888:3888 --name kafka --rm -td kafkarm'
 alias dstop='sudo docker stop kafka'
 alias dexec='sudo docker exec -u kafka -it kafka bash'
 alias dexecr='sudo docker exec -u root -it kafka bash'
